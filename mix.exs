8e976b4fab26ada3ae4179644949f65b5d08a8ce
Mayel <git@mayel.space>
2021-04-10 16:05:24 +0200

    clean

--- mix.exs
+++ mix.exs
@@ -9,17 +9,17 @@ defmodule Bonfire.Data.AccessControl.MixProject do
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       description: "Access Control models for commonspub",
-      homepage_url: "https://github.com/bonfire-ecosystem/bonfire_data_access_control",
-      source_url: "https://github.com/bonfire-ecosystem/bonfire_data_access_control",
+      homepage_url: "https://github.com/bonfire-networks/bonfire_data_access_control",
+      source_url: "https://github.com/bonfire-networks/bonfire_data_access_control",
       package: [
         licenses: ["MPL 2.0"],
         links: %{
-          "Repository" => "https://github.com/bonfire-ecosystem/bonfire_data_access_control",
+          "Repository" => "https://github.com/bonfire-networks/bonfire_data_access_control",
           "Hexdocs" => "https://hexdocs.pm/bonfire_data_access_control",
         },
       ],
       docs: [
-        main: "readme", # The first page to display from the docs 
+        main: "readme", # The first page to display from the docs
         extras: ["README.md"], # extra pages to include
       ],
       deps: Mess.deps [{:ex_doc, ">= 0.0.0", only: :dev, runtime: false}]
